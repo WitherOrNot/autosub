75d387e39316b7033d850c647ff11c59630a24c7
Wither OrNot <damemem@gmail.com>
2020-07-02 23:21:35 -0400

    improve

--- __main__.py
+++ __main__.py
@@ -1,10 +1,15 @@
-from autosub import add_subs
+from autosub import add_subs, get_langs
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("video", help="Video to subtitle")
 parser.add_argument("--lang", "-l", help="Languages to subtitle", action="store", nargs="+", default=["en"])
+parser.add_argument("--available", "-a", action="store_true", default=False)
 args = parser.parse_args()
+
+if args.available:
+    print(",".join(get_langs(args.video)))
+    quit()
 
 for l in args.lang:
     add_subs(args.video, lang=l)
